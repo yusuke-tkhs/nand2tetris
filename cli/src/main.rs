0105b030509ecd0b878979c1375b458bf51fcf1b
Yusuke Takahashi <yusuke_takahashi@caddi.jp>
2022-11-20 14:20:28 +0900

    finish cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,25 @@
+use core::panic;
+use std::path::{Path, PathBuf};
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let input_path = (&args).get(1).unwrap(); // 入力されるアセンブラ言語のパス
-    let output_path = (&args).get(1).unwrap(); // アセンブラ言語から生成された機械語を生成するパス
+    // 入力されるアセンブラ言語のパス
+    let input_path: &Path = Path::new(args.get(1).unwrap());
+
+    // .asm 以外はエラーにする
+    if input_path.extension().unwrap() != "asm" {
+        panic!("input file format must be .asm");
+    }
+
+    // アセンブラ言語から生成された機械語を生成するパス
+    let output_path: PathBuf = {
+        let mut path = std::path::PathBuf::from(input_path.parent().unwrap());
+        path.push(format!(
+            "{}.hack",
+            input_path.file_stem().unwrap().to_str().unwrap()
+        ));
+        path
+    };
+
     dbg!(input_path);
     dbg!(output_path);
 }
