f61bc32a8b98289ea9c7b1d687729994112f6410
Yusuke Takahashi <yusuke_takahashi@caddi.jp>
2022-11-27 00:54:11 +0900

    add virtual machine language schema

--- schema/src/lib.rs
+++ schema/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod hack;
+pub mod vm;
---
+++ schema/src/vm.rs
@@ -0,0 +1,38 @@
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ArithmeticCommand {
+    Add,
+    Sub,
+    Neg,
+    Eq,
+    Gt,
+    Lt,
+    And,
+    Or,
+    Not,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Segment {
+    Argument,
+    Local,
+    Static,
+    Constant,
+    This,
+    That,
+    Pointer,
+    Temp,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Index(u16);
+impl Index {
+    pub fn new(v: u16) -> Self {
+        Self(v)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum MemoryAccessCommand {
+    Push(Segment, Index),
+    Pop(Segment, Index),
+}
