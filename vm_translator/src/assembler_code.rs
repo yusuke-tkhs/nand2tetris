3493e8ba9eae1aeeba7a29574085c2393bd7c3a0
Yusuke Takahashi <yusuke_takahashi@caddi.jp>
2022-11-27 23:51:14 +0900

    add sleketon impl of memory access command

--- vm_translator/src/assembler_code.rs
+++ vm_translator/src/assembler_code.rs
@@ -1,11 +1,47 @@
 use schema::{hack, vm};
+
+const STACK_BASE_ADDRESS: u16 = 256;
+
 pub fn construct(input: Vec<vm::Command>) -> anyhow::Result<Vec<hack::Command>> {
     Ok(input
         .into_iter()
         .flat_map(|vm_command: vm::Command| -> Vec<hack::Command> {
             match vm_command {
-                vm::Command::MemoryAccess(_memory_access_command) => {
-                    unimplemented!()
+                vm::Command::MemoryAccess(memory_access_command) => {
+                    match memory_access_command {
+                        vm::MemoryAccessCommand::Push(segment, index) => {
+                            match segment {
+                                vm::Segment::Argument => {
+                                    // // Argumentベースアドレス取得
+                                    // @ARG
+                                    // D=M
+                                    // // ベースアドレスにインデックスを加算して対象とするArgumentのアドレスを求める
+                                    // // 対象とするArgumentのメモリ位置にある値をDレジスタに格納する
+                                    // @index
+                                    // A=D+A
+                                    // D=M
+                                    // // スタックポインタが示すメモリ位置に引数の値を格納する
+                                    // @SP
+                                    // A=M
+                                    // M=D
+                                    vec![hack::Command::A(hack::ACommand::Symbol(
+                                        hack::Symbol::new("ARG"),
+                                    ))];
+                                }
+                                vm::Segment::Local => {}
+                                vm::Segment::Static => {}
+                                vm::Segment::Constant => {}
+                                vm::Segment::This => {}
+                                vm::Segment::That => {}
+                                vm::Segment::Pointer => {}
+                                vm::Segment::Temp => {}
+                            }
+                            unimplemented!()
+                        }
+                        vm::MemoryAccessCommand::Pop(segment, index) => {
+                            unimplemented!()
+                        }
+                    }
                 }
                 vm::Command::Arithmetic(_arithmetic_command) => {
                     unimplemented!()
