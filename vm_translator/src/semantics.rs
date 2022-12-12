f0b417abc33c7a8aec4c73beeaade504396a6abc
Yusuke Takahashi <yusuke_takahashi@caddi.jp>
2022-12-13 00:56:29 +0900

    split impl of semantics

--- vm_translator/src/semantics.rs
+++ vm_translator/src/semantics.rs
@@ -1,5 +1,4 @@
-use schema::vm;
-
+mod from_schema;
 // ファイルはモジュールと仮定する
 #[allow(dead_code)]
 pub struct Module {
@@ -8,52 +7,10 @@ pub struct Module {
 }
 
 #[allow(dead_code)]
-impl Module {
-    fn try_from_commands(
-        module_name: String,
-        vm_commands: Vec<vm::Command>,
-    ) -> anyhow::Result<Self> {
-        Ok(Self {
-            name: module_name,
-            functions: Function::try_from_commands(vm_commands)?,
-        })
-    }
-}
-
-#[allow(dead_code)]
 pub struct Function {
     name: String,
     args_count: u16,
     commands: Vec<Command>,
-}
-
-#[allow(dead_code)]
-impl Function {
-    fn try_from_commands(vm_commands: Vec<vm::Command>) -> anyhow::Result<Vec<Self>> {
-        let each_function_vm_commands = separate(vm_commands, |vm_command| {
-            matches!(vm_command, vm::Command::Function { .. })
-        });
-        each_function_vm_commands
-            .into_iter()
-            .map(|commands| {
-                let Some((
-                    vm::Command::Function { name, args_count },
-                    rest_commands
-                )) = commands.split_first() else {
-                    anyhow::bail!("all commands should be written in function!");
-                };
-                Ok(Self {
-                    name: name.get().to_string(),
-                    args_count: *args_count,
-                    commands: rest_commands
-                        .iter()
-                        .cloned()
-                        .map(Command::try_from_command)
-                        .collect::<anyhow::Result<Vec<_>>>()?,
-                })
-            })
-            .collect::<anyhow::Result<Vec<_>>>()
-    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -67,71 +24,10 @@ pub enum Command {
     IfGoto(String),
 }
 
-impl Command {
-    pub fn try_from_command(src: vm::Command) -> anyhow::Result<Self> {
-        Ok(match src {
-            vm::Command::Function { .. } => {
-                anyhow::bail!("function could not converted to semantic command")
-            }
-            vm::Command::Arithmetic(arithmetic_command) => Self::Arithmetic(
-                ArithmeticCommand::from_arithmetic_command(arithmetic_command),
-            ),
-            vm::Command::MemoryAccess(memory_access_command) => Self::MemoryAccess({
-                match memory_access_command.access_type {
-                    vm::AccessType::Push => MemoryAccessCommand::Push(
-                        PushSource::from_memory_access_command(memory_access_command),
-                    ),
-                    vm::AccessType::Pop => MemoryAccessCommand::Pop(
-                        PopTarget::try_from_memory_access_command(memory_access_command)?,
-                    ),
-                }
-            }),
-            vm::Command::Call { name, args_count } => Command::Call {
-                name: name.get_string(),
-                args_count,
-            },
-            vm::Command::Return => Command::Return,
-            vm::Command::Label(label) => Command::Label(label.get_string()),
-            vm::Command::Goto(label) => Command::Goto(label.get_string()),
-            vm::Command::IfGoto(label) => Command::IfGoto(label.get_string()),
-        })
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum ArithmeticCommand {
     UnaryOperator(UnaryOperator),
     BinaryOperator(BinaryOperator),
-}
-
-impl ArithmeticCommand {
-    fn from_arithmetic_command(command: vm::ArithmeticCommand) -> Self {
-        match command {
-            vm::ArithmeticCommand::Add => ArithmeticCommand::BinaryOperator(
-                BinaryOperator::Mathmatical(BinaryMathmaticalOperator::Addition),
-            ),
-            vm::ArithmeticCommand::Sub => ArithmeticCommand::BinaryOperator(
-                BinaryOperator::Mathmatical(BinaryMathmaticalOperator::Sububraction),
-            ),
-            vm::ArithmeticCommand::Neg => ArithmeticCommand::UnaryOperator(UnaryOperator::Negative),
-            vm::ArithmeticCommand::Eq => ArithmeticCommand::BinaryOperator(
-                BinaryOperator::Comparison(BinaryComparisonOperator::Equal),
-            ),
-            vm::ArithmeticCommand::Gt => ArithmeticCommand::BinaryOperator(
-                BinaryOperator::Comparison(BinaryComparisonOperator::GreaterThan),
-            ),
-            vm::ArithmeticCommand::Lt => ArithmeticCommand::BinaryOperator(
-                BinaryOperator::Comparison(BinaryComparisonOperator::LessThan),
-            ),
-            vm::ArithmeticCommand::And => ArithmeticCommand::BinaryOperator(
-                BinaryOperator::Logical(BinaryLogicalOperator::And),
-            ),
-            vm::ArithmeticCommand::Or => ArithmeticCommand::BinaryOperator(
-                BinaryOperator::Logical(BinaryLogicalOperator::Or),
-            ),
-            vm::ArithmeticCommand::Not => ArithmeticCommand::UnaryOperator(UnaryOperator::Not),
-        }
-    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -189,39 +85,6 @@ pub enum PushSource {
     },
 }
 
-impl PushSource {
-    fn from_memory_access_command(src: vm::MemoryAccessCommand) -> Self {
-        match src.segment {
-            vm::Segment::Argument => Self::IndirectAddress {
-                mapping_type: InDirectMappingType::Argument,
-                offset: src.index.get(),
-            },
-            vm::Segment::Local => Self::IndirectAddress {
-                mapping_type: InDirectMappingType::Local,
-                offset: src.index.get(),
-            },
-            vm::Segment::Static => Self::StaticVariable(src.index.get()),
-            vm::Segment::Constant => Self::Constant(src.index.get()),
-            vm::Segment::This => Self::IndirectAddress {
-                mapping_type: InDirectMappingType::This,
-                offset: src.index.get(),
-            },
-            vm::Segment::That => Self::IndirectAddress {
-                mapping_type: InDirectMappingType::That,
-                offset: src.index.get(),
-            },
-            vm::Segment::Pointer => Self::DirectAddress {
-                mapping_type: DirectMappingType::Pointer,
-                offset: src.index.get(),
-            },
-            vm::Segment::Temp => Self::DirectAddress {
-                mapping_type: DirectMappingType::Temp,
-                offset: src.index.get(),
-            },
-        }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum PopTarget {
     StaticVariable(u16), // スタティック変数
@@ -237,39 +100,6 @@ pub enum PopTarget {
     },
 }
 
-impl PopTarget {
-    fn try_from_memory_access_command(src: vm::MemoryAccessCommand) -> anyhow::Result<Self> {
-        Ok(match src.segment {
-            vm::Segment::Argument => Self::IndirectAddress {
-                mapping_type: InDirectMappingType::Argument,
-                offset: src.index.get(),
-            },
-            vm::Segment::Local => Self::IndirectAddress {
-                mapping_type: InDirectMappingType::Local,
-                offset: src.index.get(),
-            },
-            vm::Segment::Static => Self::StaticVariable(src.index.get()),
-            vm::Segment::Constant => anyhow::bail!("catnnot pop to constant"),
-            vm::Segment::This => Self::IndirectAddress {
-                mapping_type: InDirectMappingType::This,
-                offset: src.index.get(),
-            },
-            vm::Segment::That => Self::IndirectAddress {
-                mapping_type: InDirectMappingType::That,
-                offset: src.index.get(),
-            },
-            vm::Segment::Pointer => Self::DirectAddress {
-                mapping_type: DirectMappingType::Pointer,
-                offset: src.index.get(),
-            },
-            vm::Segment::Temp => Self::DirectAddress {
-                mapping_type: DirectMappingType::Temp,
-                offset: src.index.get(),
-            },
-        })
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum DirectMappingType {
     Pointer,
@@ -282,33 +112,4 @@ pub enum InDirectMappingType {
     Local,
     This,
     That,
-}
-
-// pred が true を返す度に新しいVecとなるように分割する
-fn separate<T>(vec: Vec<T>, pred: fn(&T) -> bool) -> Vec<Vec<T>> {
-    let mut res: Vec<Vec<T>> = vec![];
-    for v in vec {
-        if pred(&v) {
-            res.push(vec![v]);
-        } else if let Some(last) = res.last_mut() {
-            last.push(v);
-        } else {
-            res.push(vec![v]);
-        }
-    }
-    res
-}
-
-#[test]
-fn test_separete() {
-    let nums = vec![0, 1, 2, 3, 4, 5, 6, 7];
-    assert_eq!(
-        separate(nums, |n| n % 3 == 0),
-        vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7],]
-    );
-    let nums = vec![1, 2, 3, 4, 5, 6, 7];
-    assert_eq!(
-        separate(nums, |n| n % 3 == 0),
-        vec![vec![1, 2], vec![3, 4, 5], vec![6, 7],]
-    )
 }
---
+++ vm_translator/src/semantics/from_schema.rs
@@ -0,0 +1,200 @@
+use super::*;
+use schema::vm;
+
+#[allow(dead_code)]
+impl Module {
+    fn try_from_commands(
+        module_name: String,
+        vm_commands: Vec<vm::Command>,
+    ) -> anyhow::Result<Self> {
+        Ok(Self {
+            name: module_name,
+            functions: Function::try_from_commands(vm_commands)?,
+        })
+    }
+}
+
+#[allow(dead_code)]
+impl Function {
+    fn try_from_commands(vm_commands: Vec<vm::Command>) -> anyhow::Result<Vec<Self>> {
+        let each_function_vm_commands = separate(vm_commands, |vm_command| {
+            matches!(vm_command, vm::Command::Function { .. })
+        });
+        each_function_vm_commands
+            .into_iter()
+            .map(|commands| {
+                let Some((
+                    vm::Command::Function { name, args_count },
+                    rest_commands
+                )) = commands.split_first() else {
+                    anyhow::bail!("all commands should be written in function!");
+                };
+                Ok(Self {
+                    name: name.get().to_string(),
+                    args_count: *args_count,
+                    commands: rest_commands
+                        .iter()
+                        .cloned()
+                        .map(Command::try_from_command)
+                        .collect::<anyhow::Result<Vec<_>>>()?,
+                })
+            })
+            .collect::<anyhow::Result<Vec<_>>>()
+    }
+}
+
+impl Command {
+    pub fn try_from_command(src: vm::Command) -> anyhow::Result<Self> {
+        Ok(match src {
+            vm::Command::Function { .. } => {
+                anyhow::bail!("function could not converted to semantic command")
+            }
+            vm::Command::Arithmetic(arithmetic_command) => Self::Arithmetic(
+                ArithmeticCommand::from_arithmetic_command(arithmetic_command),
+            ),
+            vm::Command::MemoryAccess(memory_access_command) => Self::MemoryAccess({
+                match memory_access_command.access_type {
+                    vm::AccessType::Push => MemoryAccessCommand::Push(
+                        PushSource::from_memory_access_command(memory_access_command),
+                    ),
+                    vm::AccessType::Pop => MemoryAccessCommand::Pop(
+                        PopTarget::try_from_memory_access_command(memory_access_command)?,
+                    ),
+                }
+            }),
+            vm::Command::Call { name, args_count } => Command::Call {
+                name: name.get_string(),
+                args_count,
+            },
+            vm::Command::Return => Command::Return,
+            vm::Command::Label(label) => Command::Label(label.get_string()),
+            vm::Command::Goto(label) => Command::Goto(label.get_string()),
+            vm::Command::IfGoto(label) => Command::IfGoto(label.get_string()),
+        })
+    }
+}
+
+impl ArithmeticCommand {
+    fn from_arithmetic_command(command: vm::ArithmeticCommand) -> Self {
+        match command {
+            vm::ArithmeticCommand::Add => ArithmeticCommand::BinaryOperator(
+                BinaryOperator::Mathmatical(BinaryMathmaticalOperator::Addition),
+            ),
+            vm::ArithmeticCommand::Sub => ArithmeticCommand::BinaryOperator(
+                BinaryOperator::Mathmatical(BinaryMathmaticalOperator::Sububraction),
+            ),
+            vm::ArithmeticCommand::Neg => ArithmeticCommand::UnaryOperator(UnaryOperator::Negative),
+            vm::ArithmeticCommand::Eq => ArithmeticCommand::BinaryOperator(
+                BinaryOperator::Comparison(BinaryComparisonOperator::Equal),
+            ),
+            vm::ArithmeticCommand::Gt => ArithmeticCommand::BinaryOperator(
+                BinaryOperator::Comparison(BinaryComparisonOperator::GreaterThan),
+            ),
+            vm::ArithmeticCommand::Lt => ArithmeticCommand::BinaryOperator(
+                BinaryOperator::Comparison(BinaryComparisonOperator::LessThan),
+            ),
+            vm::ArithmeticCommand::And => ArithmeticCommand::BinaryOperator(
+                BinaryOperator::Logical(BinaryLogicalOperator::And),
+            ),
+            vm::ArithmeticCommand::Or => ArithmeticCommand::BinaryOperator(
+                BinaryOperator::Logical(BinaryLogicalOperator::Or),
+            ),
+            vm::ArithmeticCommand::Not => ArithmeticCommand::UnaryOperator(UnaryOperator::Not),
+        }
+    }
+}
+
+impl PushSource {
+    fn from_memory_access_command(src: vm::MemoryAccessCommand) -> Self {
+        match src.segment {
+            vm::Segment::Argument => Self::IndirectAddress {
+                mapping_type: InDirectMappingType::Argument,
+                offset: src.index.get(),
+            },
+            vm::Segment::Local => Self::IndirectAddress {
+                mapping_type: InDirectMappingType::Local,
+                offset: src.index.get(),
+            },
+            vm::Segment::Static => Self::StaticVariable(src.index.get()),
+            vm::Segment::Constant => Self::Constant(src.index.get()),
+            vm::Segment::This => Self::IndirectAddress {
+                mapping_type: InDirectMappingType::This,
+                offset: src.index.get(),
+            },
+            vm::Segment::That => Self::IndirectAddress {
+                mapping_type: InDirectMappingType::That,
+                offset: src.index.get(),
+            },
+            vm::Segment::Pointer => Self::DirectAddress {
+                mapping_type: DirectMappingType::Pointer,
+                offset: src.index.get(),
+            },
+            vm::Segment::Temp => Self::DirectAddress {
+                mapping_type: DirectMappingType::Temp,
+                offset: src.index.get(),
+            },
+        }
+    }
+}
+
+impl PopTarget {
+    fn try_from_memory_access_command(src: vm::MemoryAccessCommand) -> anyhow::Result<Self> {
+        Ok(match src.segment {
+            vm::Segment::Argument => Self::IndirectAddress {
+                mapping_type: InDirectMappingType::Argument,
+                offset: src.index.get(),
+            },
+            vm::Segment::Local => Self::IndirectAddress {
+                mapping_type: InDirectMappingType::Local,
+                offset: src.index.get(),
+            },
+            vm::Segment::Static => Self::StaticVariable(src.index.get()),
+            vm::Segment::Constant => anyhow::bail!("catnnot pop to constant"),
+            vm::Segment::This => Self::IndirectAddress {
+                mapping_type: InDirectMappingType::This,
+                offset: src.index.get(),
+            },
+            vm::Segment::That => Self::IndirectAddress {
+                mapping_type: InDirectMappingType::That,
+                offset: src.index.get(),
+            },
+            vm::Segment::Pointer => Self::DirectAddress {
+                mapping_type: DirectMappingType::Pointer,
+                offset: src.index.get(),
+            },
+            vm::Segment::Temp => Self::DirectAddress {
+                mapping_type: DirectMappingType::Temp,
+                offset: src.index.get(),
+            },
+        })
+    }
+}
+
+// pred が true を返す度に新しいVecとなるように分割する
+fn separate<T>(vec: Vec<T>, pred: fn(&T) -> bool) -> Vec<Vec<T>> {
+    let mut res: Vec<Vec<T>> = vec![];
+    for v in vec {
+        if pred(&v) {
+            res.push(vec![v]);
+        } else if let Some(last) = res.last_mut() {
+            last.push(v);
+        } else {
+            res.push(vec![v]);
+        }
+    }
+    res
+}
+
+#[test]
+fn test_separete() {
+    let nums = vec![0, 1, 2, 3, 4, 5, 6, 7];
+    assert_eq!(
+        separate(nums, |n| n % 3 == 0),
+        vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7],]
+    );
+    let nums = vec![1, 2, 3, 4, 5, 6, 7];
+    assert_eq!(
+        separate(nums, |n| n % 3 == 0),
+        vec![vec![1, 2], vec![3, 4, 5], vec![6, 7],]
+    )
+}
