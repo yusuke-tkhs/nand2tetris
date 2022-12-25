6c728304cb920e983dec8326c04353a27cb0b7c0
Yusuke Takahashi <yusuke_takahashi@caddi.jp>
2022-12-26 01:30:10 +0900

    parsable enum

--- schema/src/parser.rs
+++ schema/src/parser.rs
@@ -1,13 +1,72 @@
 use crate::constant::DIGIT_CHAR;
+use combine::attempt;
 use combine::error::StreamError;
 use combine::one_of;
 use combine::parser;
+use combine::parser::char::string;
+use combine::parser::choice::choice;
+use combine::parser::token::value;
 use combine::stream::RangeStream;
 use combine::stream::StreamErrorFor;
 use combine::stream::StreamOnce;
 use combine::EasyParser;
 use combine::Stream;
 use combine::{many, many1};
+
+#[macro_export]
+macro_rules! parsable_enum{
+    (
+        $(#[$attr:meta])*
+        $enum_vis: vis enum $enum_name: ident {
+            $(
+                $case_name: ident: $case_string: literal
+            ),+$(,)?
+        }
+    ) => {
+        $(#[$attr])*
+        $enum_vis enum $enum_name {
+            $($case_name),+
+        }
+
+        impl $enum_name {
+            $enum_vis fn parser<Input>() -> impl combine::Parser<Input, Output = $enum_name>
+            where Input: Stream<Token = char>
+            {
+                parser! {
+                    fn inner_fn[Input]()(Input) -> $enum_name
+                    where [Input: Stream<Token = char>]
+                    {
+                        choice([
+                            $(attempt(string($case_string).with(value($enum_name::$case_name)))),+
+                        ])
+                    }
+                }
+                inner_fn()
+            }
+            $enum_vis fn to_str(&self) -> &str {
+                match self {
+                    $(Self::$case_name => $case_string),+
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parsable_enum() {
+    use tests::easy_parser_assert;
+    parsable_enum! {
+        #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+        pub enum Test {
+            A: "a",
+            B: "b_dayo!#$%",
+        }
+    };
+    easy_parser_assert(Test::parser, "a", Test::A);
+    easy_parser_assert(Test::parser, "b_dayo!#$%", Test::B);
+    assert_eq!(Test::A.to_str(), "a");
+    assert_eq!(Test::B.to_str(), "b_dayo!#$%");
+}
 
 parser! {
     pub(crate) fn p_u16[Input]()(Input) -> u16
