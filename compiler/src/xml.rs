2e89eb6be44059f4a0504f8aca648bf6764e1b9e
Yusuke Takahashi <yusuke_takahashi@caddi.jp>
2023-01-01 17:42:06 +0900

    petit refactor

--- compiler/src/xml.rs
+++ compiler/src/xml.rs
@@ -2,21 +2,17 @@ use schema::jack::Token;
 
 pub fn tokens_to_xml(tokens: Vec<Token>) -> String {
     std::iter::once("<tokens>".to_string())
-        .chain(
-            tokens
-                .into_iter()
-                .map(|token| match token {
-                    Token::Keyword(keyword) => ("keyword", keyword.as_str().to_string()),
-                    Token::Symbol(symbol) => ("symbol", symbol.as_str().to_string()),
-                    Token::IntegerConstant(v) => ("integerConstant", v.to_string()),
-                    Token::StringConstant(s) => ("stringConstant", s),
-                    Token::Identifier(s) => ("identifier", s),
-                })
-                .map(|(key_name, value)| {
-                    let sanitized_value = sanitize(value);
-                    format!("<{key_name}> {sanitized_value} </{key_name}>")
-                }),
-        )
+        .chain(tokens.into_iter().map(|token| {
+            let (key_name, value) = match token {
+                Token::Keyword(keyword) => ("keyword", keyword.as_str().to_string()),
+                Token::Symbol(symbol) => ("symbol", symbol.as_str().to_string()),
+                Token::IntegerConstant(v) => ("integerConstant", v.to_string()),
+                Token::StringConstant(s) => ("stringConstant", s),
+                Token::Identifier(s) => ("identifier", s),
+            };
+            let sanitized_value = sanitize(value);
+            format!("<{key_name}> {sanitized_value} </{key_name}>")
+        }))
         .chain(std::iter::once("</tokens>".to_string()))
         .collect::<Vec<_>>()
         .join("\n")
