4e4fb104dc995aec72298f47e4ecc35e0d08bb8b
Yusuke Takahashi <yusuke_takahashi@caddi.jp>
2023-01-14 17:49:34 +0900

    add skeleton impl of output vm function

--- compiler/src/codegen.rs
+++ compiler/src/codegen.rs
@@ -1,5 +1,23 @@
 use schema::{jack::token_analyzer::*, vm};
-pub fn class_to_commands(_class: &Class) -> Vec<vm::Command> {
+pub fn class_to_commands(class: &Class) -> Vec<vm::Command> {
+    class
+        .variable_declearations
+        .iter()
+        .flat_map(class_var_dec_to_commands)
+        .chain(
+            class
+                .subroutine_declerations
+                .iter()
+                .flat_map(subroutine_dec_to_commands),
+        )
+        .collect()
+}
+
+fn class_var_dec_to_commands(_class_var_dec: &ClassVariableDecleration) -> Vec<vm::Command> {
+    unimplemented!()
+}
+
+fn subroutine_dec_to_commands(_subroutine_dec: &ClassSubroutineDecleration) -> Vec<vm::Command> {
     unimplemented!()
 }
 
